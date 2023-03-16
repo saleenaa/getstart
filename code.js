42f6a563e08240d6d01bc1ba905fd351358fa083
saleenaa <127310601+saleenaa@users.noreply.github.com>
2023-03-16 17:18:44 +0100

    Delete code.js

--- code.js
+++
@@ -1,7 +0,0 @@
-const demo = document.querySelector(".demo");
-
-
-demo.addEventListener("click", ({ target }) => {
-    const bloc = target.closest("li");
-    bloc.classList.toggle("overlay");
-}, { passive: true });
